c86118fb0e6af6efd84e88e5d894c6248d65d97f
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-03-12 22:03:18 +0700

    menambahkan implementasi dengan state-based construction
    Signed-off-by: Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>

--- tpmodul4_1302213038/tpmodul4_1302213038/Program.cs
+++ tpmodul4_1302213038/tpmodul4_1302213038/Program.cs
@@ -10,3 +10,55 @@ public class KodePos
         return kodePos[(int)kelurahan];
     }
 }
+
+public class DoorMachine
+{
+    enum State { terkunci, terbuka };
+
+    public static void Main()
+    {
+        KodePos kodePos = new KodePos();
+        int getKdp = KodePos.getKodePos(KodePos.lurah.Kujangsari);
+        Console.WriteLine("Kode Pos kelurahan Kujangsari adalah " + getKdp + ".\n");
+
+        State state = State.terkunci;
+        string[] doorLabel = { "Pintu terkunci", "Pintu tidak terkunci"};
+
+        while (state != null)
+        {
+            Console.WriteLine(doorLabel[(int)state]);
+            Console.Write("Input Wanted State: ");
+
+            string input = Console.ReadLine();
+            switch (state)
+            {
+                case State.terkunci:
+                    if (input == "KunciPintu")
+                    {
+                        state = State.terbuka;
+                    } else if (input == "BukaPintu") {
+                        state = State.terbuka;
+                    } else
+                    {
+                        state = State.terkunci;
+                        Console.WriteLine("Invalid input!\n");
+                    }
+                    break;
+
+                case State.terbuka:
+                    if (input == "KunciPintu")
+                    {
+                        state = State.terkunci;
+                    } else if (input == "BukaPintu")
+                    {
+                        state = State.terbuka;
+                    } else
+                    {
+                        state = State.terbuka;
+                        Console.WriteLine("Invalid input!\n");
+                    }
+                    break;
+            }
+        }
+    }
+}
