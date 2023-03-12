ee1f2c26d25721446af70b83c9c33798756b2473
Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>
2023-03-12 20:44:53 +0700

    menambahkan implementasi dengan table-driven
    Signed-off-by: Aisha Nuryan <91200696+aishaptrn@users.noreply.github.com>

--- tpmodul4_1302213038/tpmodul4_1302213038/Program.cs
+++ tpmodul4_1302213038/tpmodul4_1302213038/Program.cs
@@ -1,2 +1,12 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using System.Security.Cryptography.X509Certificates;
+
+public class KodePos
+{
+    public enum lurah { Batununggal, Kujangsari, Mengger, Wates, Cijawura, Jatisari, Margasari, Sekejati, Kebonwaru, Maleer, Samoja}
+    public static int getKodePos(lurah kelurahan)
+    {
+        int[] kodePos = { 40266, 40287, 40267, 402562, 40287, 40286, 40286, 40286, 40272, 40274, 40273 };
+        return kodePos[(int)kelurahan];
+    }
+}
